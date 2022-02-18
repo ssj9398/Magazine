14f3c3f31c6cc9d4c5179e5ddffa65715e3890bb
alisyabob <sjs939883@gmail.com>
2022-02-18 21:12:56 +0900

    [TEST]  게시글 전체조회 테스트코드 작성 완료

--- src/test/java/week/pro/service/BoardServiceTest.java
+++ src/test/java/week/pro/service/BoardServiceTest.java
@@ -14,6 +14,7 @@ import week.pro.repository.AccountRepository;
 import week.pro.repository.BoardRepository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -53,4 +54,28 @@ class BoardServiceTest {
         Assertions.assertThat(findBoardId).isEqualTo(findId.get().getId());
 
     }
+
+    @Test
+    public void 게시글전체조회() throws Exception {
+        //given
+        AccountRequestDto accountRequestDto = new AccountRequestDto("홍길동님", "abcd@google.com", "1234");
+        accountService.addUser(accountRequestDto);
+        em.flush();
+
+        //when
+        BoardRequestDto boardRequestDto = new BoardRequestDto("abcd@google.com","내용내용");
+        Long findBoardId = boardService.addBoard(boardRequestDto);
+        BoardRequestDto boardRequestDto2 = new BoardRequestDto("abcd@google.com","내용내용2");
+        Long findBoardId2 = boardService.addBoard(boardRequestDto2);
+        BoardRequestDto boardRequestDto3 = new BoardRequestDto("abcd@google.com","내용내용3");
+        Long findBoardId3 = boardService.addBoard(boardRequestDto3);
+
+        em.flush();
+
+        //then
+        List<Board> boardList = boardService.findBoard();
+        System.out.println(boardList);
+        Assertions.assertThat(boardList.size()).isEqualTo(3);
+    }
+
 }
