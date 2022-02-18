f40402dc8f49d514c98b62cd5e38f77b3abb762d
alisyabob <sjs939883@gmail.com>
2022-02-19 03:07:51 +0900

    [FEAT] 게시글 테이블 회원id 조회 쿼리 추가

--- src/main/java/week/pro/repository/BoardRepository.java
+++ src/main/java/week/pro/repository/BoardRepository.java
@@ -1,7 +1,13 @@
 package week.pro.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import week.pro.domain.Board;
 
+import java.util.Optional;
+
 public interface BoardRepository extends JpaRepository<Board,Long> {
+    @Query("select b from Board b where b.account.id=:id")
+    Optional<Board> findByUser(@Param("id") Long id);
 }
