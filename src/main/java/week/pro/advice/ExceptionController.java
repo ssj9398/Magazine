fe088a5cec092ad2da4628004885cb5fee618af6
alisyabob <sjs939883@gmail.com>
2022-02-22 03:23:26 +0900

    [FEAT] Exception

--- src/main/java/week/pro/advice/ExceptionController.java
+++ src/main/java/week/pro/advice/ExceptionController.java
@@ -6,9 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import week.pro.advice.exception.BoardNotFoundException;
-import week.pro.advice.exception.UserNameDuplicateException;
-import week.pro.advice.exception.UserNotFoundException;
+import week.pro.advice.exception.*;
 import week.pro.model.Fail;
 
 @RequiredArgsConstructor
@@ -29,5 +27,15 @@ public class ExceptionController {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Fail> UserNotFoundException(UserNotFoundException e) {
         return new ResponseEntity<>(new Fail("해당 사용자가 존재하지 않습니다."), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(LikeBoardCreateException.class)
+    public ResponseEntity<Fail> LikeBoardNotFoundException(LikeBoardCreateException e) {
+        return new ResponseEntity<>(new Fail("게시글을 좋아요 할 수 없습니다."), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(LikeNotFoundException.class)
+    public ResponseEntity<Fail> LikeNotFoundException(LikeNotFoundException e) {
+        return new ResponseEntity<>(new Fail("게시글 좋아요를 삭제 할 수 없습니다."), HttpStatus.OK);
     }
 }
