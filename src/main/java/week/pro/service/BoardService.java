0095e4a92e0be0e8c98d9bbc2b6f77933970a3e2
alisyabob <sjs939883@gmail.com>
2022-02-25 01:09:34 +0900

    [FEAT] 게시글 삭제로직 구현 완료

--- src/main/java/week/pro/service/BoardService.java
+++ src/main/java/week/pro/service/BoardService.java
@@ -44,4 +44,9 @@ public class BoardService {
         return boardOne;
     }
 
+    @Transactional
+    public void removeBoard(Long boardId){
+        boardRepository.deleteById(boardId);
+    }
+
 }
