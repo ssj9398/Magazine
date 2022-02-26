1e2665e3073d25a037d92e93e951aa31942f3c16
alisyabob <sjs939883@gmail.com>
2022-02-27 01:26:07 +0900

    [FEAT] 게시글 수정 및 삭제 웹Layer 로직 구현

--- src/main/java/week/pro/controller/BoardController.java
+++ src/main/java/week/pro/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import week.pro.advice.GetAllBoard;
 import week.pro.advice.GetBoard;
+import week.pro.advice.Success;
 import week.pro.domain.Account;
 import week.pro.dto.BoardRequestDto;
 import week.pro.dto.BoardResponseDto;
@@ -32,5 +33,17 @@ public class BoardController {
     public ResponseEntity<GetAllBoard> AllBoardList(){
         List<BoardResponseDto> findAllBoard = boardService.findAllBoard();
         return new ResponseEntity<>(new GetAllBoard(true, "전체 게시글 조회 성공", findAllBoard),HttpStatus.OK);
+    }
+
+    @PutMapping("/board/{boardId}")
+    public ResponseEntity<Success> boardModify(@PathVariable Long boardId, @RequestBody BoardRequestDto.BoardModify boardModify){
+        boardService.modifyBoard(boardId, boardModify);
+        return new ResponseEntity<>(new Success(true,"게시글 수정 성공"),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/board/{boardId}")
+    public ResponseEntity<Success> boardRemove(@PathVariable Long boardId){
+        boardService.removeBoard(boardId);
+        return new ResponseEntity<>(new Success(true,"게시글 삭제 성공"),HttpStatus.OK);
     }
 }
