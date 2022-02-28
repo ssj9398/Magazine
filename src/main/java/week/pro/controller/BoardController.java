37a725a0c63468b31d1cde2aa5039c53719d8137
alisyabob <sjs939883@gmail.com>
2022-02-28 12:56:40 +0900

    [FEAT] 게시글 수정 삭제 시 본인글 체크 로직 추가 및 게시글 페이징 처리

--- src/main/java/week/pro/controller/BoardController.java
+++ src/main/java/week/pro/controller/BoardController.java
@@ -1,16 +1,20 @@
 package week.pro.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import week.pro.advice.GetAllBoard;
 import week.pro.advice.GetBoard;
+import week.pro.advice.GetBoardPage;
 import week.pro.advice.Success;
-import week.pro.domain.Account;
-import week.pro.dto.BoardRequestDto;
-import week.pro.dto.BoardResponseDto;
+import week.pro.dto.response.BoardPageResponseDto;
+import week.pro.dto.request.BoardRequestDto;
+import week.pro.dto.response.BoardResponseDto;
+import week.pro.jwt.UserDetailsImpl;
 import week.pro.service.BoardService;
 
 import java.util.List;
@@ -24,8 +28,9 @@ public class BoardController {
 
     @PostMapping("/board")
     public ResponseEntity<GetBoard> boardAdd(@RequestBody BoardRequestDto boardRequestDto,
-                                             @AuthenticationPrincipal Account account){
-        Long oneBoard = boardService.addBoard(account, boardRequestDto);
+                                             @AuthenticationPrincipal UserDetailsImpl account){
+        System.out.println("account.getUser()" + account.getUser());
+        Long oneBoard = boardService.addBoard(account.getUser(), boardRequestDto);
         return new ResponseEntity<>(new GetBoard(true,"게시글 등록 성공",oneBoard), HttpStatus.OK);
     }
 
@@ -35,15 +40,24 @@ public class BoardController {
         return new ResponseEntity<>(new GetAllBoard(true, "전체 게시글 조회 성공", findAllBoard),HttpStatus.OK);
     }
 
+    @GetMapping("/boardPaging")
+    public ResponseEntity<GetBoardPage> BoardPaging(Pageable pageable){
+        Page<BoardPageResponseDto> boardPaging = boardService.findBoardPaging(pageable);
+        return new ResponseEntity<>(new GetBoardPage(true,"게시글 페이징",boardPaging),HttpStatus.OK);
+    }
+
     @PutMapping("/board/{boardId}")
-    public ResponseEntity<Success> boardModify(@PathVariable Long boardId, @RequestBody BoardRequestDto.BoardModify boardModify){
-        boardService.modifyBoard(boardId, boardModify);
+    public ResponseEntity<Success> boardModify(@PathVariable Long boardId,
+                                               @RequestBody BoardRequestDto.BoardModify boardModify,
+                                               @AuthenticationPrincipal UserDetailsImpl account){
+        boardService.modifyBoard(boardId, boardModify, account.getUser().getId());
         return new ResponseEntity<>(new Success(true,"게시글 수정 성공"),HttpStatus.OK);
     }
 
     @DeleteMapping("/board/{boardId}")
-    public ResponseEntity<Success> boardRemove(@PathVariable Long boardId){
-        boardService.removeBoard(boardId);
+    public ResponseEntity<Success> boardRemove(@PathVariable Long boardId,
+                                               @AuthenticationPrincipal UserDetailsImpl account){
+        boardService.removeBoard(boardId, account.getUser().getId());
         return new ResponseEntity<>(new Success(true,"게시글 삭제 성공"),HttpStatus.OK);
     }
 }
--- src/main/java/week/pro/service/BoardService.java
+++ src/main/java/week/pro/service/BoardService.java
@@ -1,13 +1,17 @@
 package week.pro.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import week.pro.advice.exception.ApiRequestException;
 import week.pro.domain.Account;
 import week.pro.domain.Board;
-import week.pro.dto.BoardRequestDto;
-import week.pro.dto.BoardResponseDto;
-import week.pro.repository.BoardRepository;
+import week.pro.dto.response.BoardPageResponseDto;
+import week.pro.dto.request.BoardRequestDto;
+import week.pro.dto.response.BoardResponseDto;
+import week.pro.repository.board.BoardRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,7 +27,11 @@ public class BoardService {
 
     @Transactional
     public Long addBoard(Account account, BoardRequestDto boardRequestDto){
-        Long boardId = boardRepository.save(BoardRequestDto.toEntity(boardRequestDto, account)).getId();
+        System.out.println("account = " + account);
+//        if(account.getEmail().isEmpty()) {
+//            throw new ApiRequestException("토큰이 존재하지 않거나 만료된 토큰입니다.");
+//        }
+            Long boardId = boardRepository.save(BoardRequestDto.toEntity(boardRequestDto, account)).getId();
         return boardId;
     }
 
@@ -35,15 +43,28 @@ public class BoardService {
     }
 
     @Transactional
-    public Optional<Board> modifyBoard(Long boardId, BoardRequestDto.BoardModify boardModify){
+    public Optional<Board> modifyBoard(Long boardId, BoardRequestDto.BoardModify boardModify, Long accountId){
+        boardValidation(boardId,accountId);
         Optional<Board> boardOne = boardRepository.findById(boardId);
         boardOne.get().updateBoard(boardModify);
         return boardOne;
     }
 
     @Transactional
-    public void removeBoard(Long boardId){
+    public void removeBoard(Long boardId, Long accountId){
+        boardValidation(boardId,accountId);
         boardRepository.deleteById(boardId);
     }
 
+    public Page<BoardPageResponseDto> findBoardPaging(Pageable pageable) {
+        return boardRepository.findAllBoardPage(pageable);
+    }
+
+    private void boardValidation(Long boardId, Long accountId){
+        Board findBoard = boardRepository.findById(boardId).orElseThrow(()
+                -> new ApiRequestException("게시글이 존재하지 않습니다."));
+        if(!findBoard.getAccount().getId().equals(accountId)){
+            throw new ApiRequestException("게시글 작성자가 아닙니다.");
+        }
+    }
 }
